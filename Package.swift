fed49fdcfbcec7c2a849f46c1109e886962c254a
Tim Oliver <TimOliver@users.noreply.github.com>
2026-03-02 03:55:25 +0000

    Update Package.swift for 0.11.2

--- Package.swift
+++ Package.swift
@@ -21,12 +21,12 @@ let package = Package(
         .binaryTarget(
             name: "jxl",
             url: "\(baseURL)/libjxl-v\(version)-xcframework-static.zip",
-            checksum: "TODO"
+            checksum: "dc0ccce822232eb16f50a4ed0b83345d67612d40e5c8f1c0b62a992244f9be40"
         ),
         .binaryTarget(
             name: "jxl-dynamic",
             url: "\(baseURL)/libjxl-v\(version)-xcframework-dynamic.zip",
-            checksum: "TODO"
+            checksum: "1b7aa9e932e9cece8dd58a3150c1287439b544017422dee7f2b3bee2845cc9e8"
         ),
     ]
 )
